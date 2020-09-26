404afa96a787a767aa865f3b45e46ef28c085ba6
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2020-09-26 09:00:04 +0200

    Initialize using https://github.com/hashicorp/terraform-provider-scaffolding/tree/b3319b4a0e47954e3ab42f0f6db94ec6ea079089

---
+++ internal/provider/provider.go
@@ -0,0 +1,16 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func New() *schema.Provider {
+	return &schema.Provider{
+		DataSourcesMap: map[string]*schema.Resource{
+			"scaffolding_data_source": dataSourceScaffolding(),
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"scaffolding_resource": resourceScaffolding(),
+		},
+	}
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-provider-scaffolding/internal/provider"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{ProviderFunc: provider.New})
+}
